df195ee34bc733c5d1ec0eaf2b2d6c382801f869
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-03-13 21:55:16 +0530

    master

--- rotateArraybykOptimised.cpp
+++ rotateArraybykOptimised.cpp
@@ -1,60 +1,33 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-// } Driver Code Ends
-class Solution{
-	
-	
-	public:
-	void leftRotate(int arr[], int k, int n) 
-	{ 
-	   // Your code goes here
-       //OPTIMIZED APPROACH//
-	   k = k % n;
-	   reverse(arr, arr + k);
-	   reverse(arr + k, arr + n);
-	   reverse(arr, arr + n);
-	} 
-		 
+// OPTIMIZED APPROACH //
 
-};
+void leftRotate(int arr[], int n, int k) {
+    k = k % n;
+    reverse(arr, arr + k);
+    reverse(arr + k, arr + n);
+    reverse(arr, arr + n);
 
-//{ Driver Code Starts.
-
-int main() 
-{
-   	
-   
-   	int t;
-    cin >> t;
-    while (t--)
-    {
-    	int n;
-	    cin >> n;
-	    int k;
-	    cin >> k;
-	    int a[n];
-	    for(int i = 0;i<n;i++){
-	        cin >> a[i];
-	        // um[a[i]]++;
-	    }
-
-
-       
-
-        Solution ob;
-        ob.leftRotate(a,k,n);
-        
-        for (int i = 0; i < n; i++) 
-        	cout << a[i] << " "; 
-
-	    cout << "\n";
-	     
-    }
-    return 0;
 }
 
 
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
 
-// } Driver Code Ends
+    leftRotate(arr, n, k);
+
+    for (int i = 0;i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

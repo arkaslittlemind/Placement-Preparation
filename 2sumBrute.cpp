35d70f5765d0fa675540a2a54a8c6182c93be644
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-03-14 17:19:46 +0530

    master

--- 2sumBrute.cpp
+++ 2sumBrute.cpp
@@ -4,6 +4,7 @@ using namespace std;
 string read(int n, vector<int> book, int target)
 {
     // Write your code here.
+    vector<int> result;
     for(int i = 0; i < n; i++){
         for(int j = i + 1; j < n; j++){
             if(book[i] + book[j] == target){

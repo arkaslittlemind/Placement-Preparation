7d0b43afd6d760fe8302e99cbf51c48535d93f60
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-03-14 17:05:53 +0530

    master

---
+++ 2sumBrute.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string read(int n, vector<int> book, int target)
+{
+    // Write your code here.
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(book[i] + book[j] == target){
+                return "YES";
+                break;
+            }
+        }
+    }
+    return "NO";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> book(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> book[i];
+    }
+    int target;
+    cin >> target;
+    cout << read(n, book, target);
+}

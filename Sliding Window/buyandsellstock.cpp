2cef18fb5daeb2b4d6ad5b7e447d0eca373ab455
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-07-04 22:07:05 +0530

    master

--- Sliding Window/buyandsellstock.cpp
+++ Sliding Window/buyandsellstock.cpp
@@ -3,10 +3,10 @@ public:
     int maxProfit(vector<int>& prices) {
         int buy = 0, sell = 1;
         int maxProfit = 0;
-
+        int profit = 0;
         while(sell < prices.size()) {
             if(prices[buy] < prices[sell]){
-               int profit = prices[sell] - prices[buy];
+               profit = prices[sell] - prices[buy];
                maxProfit = max(profit, maxProfit);
             } else {
                 buy = sell;

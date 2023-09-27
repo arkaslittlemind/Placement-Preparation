7a4ba2f19acb2543dc1055254f608bb0e13d542d
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-28 03:08:13 +0530

    master

---
+++ C++-STL/pairs.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    pair<int, int> p1 = {1, 2};
+    cout << p1.first << " " << p1.second << endl;
+
+    pair<int, pair<int, int>> p2 = {1, {2, 3}};
+
+    cout << p2.first << " " << p2.second.second << " " << p2.second.first << endl;
+}

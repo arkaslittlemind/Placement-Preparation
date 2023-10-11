28fd499793c482953f149cab829c65b1b3db1808
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-10-11 22:06:52 +0530

    test

--- codechef.py
+++ codechef.py
@@ -1,22 +1,13 @@
-def findMinimal(N):
-    N_str = str(N)
-    n = len(N_str)
-    
-    if N % 8 == 0:
-        return N
-    
-    for i in range(n):
-        for j in range (i + 1, n):
-            for k in range(j + 1, n):
-                num = int(N_str[i] + N_str[j] + N_str[k])
-                if num % 8 == 0:
-                    return num
-                
-    return -1
+from math import isqrt
 
+def solve():
+    n = int(input().strip())
+    a = list(map(int, input().strip().split()))
+    cnt = [0, 0]
+    for x in a:
+        cnt[isqrt(x)**2 != x] += 1
+    print(cnt[0] + (1 << cnt[1].bit_length()) - 1)
 
-T = int(input())
-for _ in range(T):
-    L, N = map(int, input().split())
-    minimal = findMinimal(N)
-    print(minimal)
+t = int(input().strip())
+for _ in range(t):
+    solve()

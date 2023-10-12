69d0342674d62dc85cc74153771236cef7849a32
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-10-13 02:21:19 +0530

    runtime error solved

--- CSES Problem Set/Introductory Problems/increasingArray.cpp
+++ CSES Problem Set/Introductory Problems/increasingArray.cpp
@@ -1,47 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define lli long long int
-#define INF (int)1e18
-#define f first
-#define s second
 
-mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
-
-
-void Solve() {
-    lli n;
+int main() {
+    long long n, x, answer = 0;
     cin >> n;
-
-    vector<lli> arr(n);
-    for(lli i = 0; i < n; i++) {
-        cin >> arr[i];
+    long long values[n];
+    for (int i = 0; i < n; i++) {
+        cin >> values[i];
     }
-
-    lli count = 0; 
-    for(lli i = 1; i < n; i++) {
-        if(arr[i] <= arr[i - 1]) {
-            count += arr[i - 1] - arr[i];
-            arr[i] = arr[i - 1];
+    for (int i = 1; i < n; i++) {
+        if (values[i] < values[i - 1]) {
+            answer += (values[i - 1] - values[i]);
+            values[i] = values[i - 1];
         }
     }
-    cout << count << endl;
-}
-
-
-int32_t main()
-{
-    auto begin = std::chrono::high_resolution_clock::now();
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin >> t;
-    //while (t--)
-    //{
-        //cout << "Case #" << i << ": ";
-        Solve();
-    //}
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
-    return 0;
+    cout << answer;
 }

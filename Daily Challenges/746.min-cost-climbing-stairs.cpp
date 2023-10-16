c847c29ce721f524d1858cf8a2d3d3ea858db973
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-10-16 19:58:46 +0530

    master

---
+++ Daily Challenges/746.min-cost-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=746 lang=cpp
+ *
+ * [746] Min Cost Climbing Stairs
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        int n = cost.size();
+        for (int i = 2; i < n; i++)
+        {
+            cost[i] += min(cost[i - 1], cost[i - 2]);
+        }
+        return min(cost[n - 1], cost[n - 2]);
+    }
+};
+// @lc code=end

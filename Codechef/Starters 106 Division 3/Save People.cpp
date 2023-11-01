0938cf0c1a91962d0a3e53a512c8a9f963583c21
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-11-01 21:56:12 +0530

    Wrong Answer

---
+++ Codechef/Starters 106 Division 3/Save People.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nline "\n"
+#define INF (int)1e18
+#define f first
+#define s second
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+long long helper(long long n, long long m, long long x, long long y) {
+    long long a = min(x, m - x + 1) * n;
+    long long b = min(y, n - y + 1) * m;
+
+    long long infected = min(a, b);
+
+    return (m * n - infected);
+}
+
+void Solve() {
+    long long n, m;
+    cin >> n >> m;
+
+    long long x, y;
+    cin >> x >> y;
+
+    cout << helper(n, m, x, y) << endl;
+}
+
+
+int32_t main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        //cout << "Case #" << i << ": ";
+        Solve();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}

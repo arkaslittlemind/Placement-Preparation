0e14c3dc2af6873e54aaa6179b35976ab3312d60
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-11-08 22:01:13 +0530

    Problem C

---
+++ Codechef/Starters 107 Division 3/OR Permutation.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nline "\n"
+#define INF (int)1e18
+#define f first
+#define s second
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+
+
+
+void Solve() {
+    long long n;
+    cin >> n;
+
+    for (int i = n; i > 0; i--) {
+        cout << i << " ";
+    }
+    cout << endl;
+    
+}
+
+int32_t main() {
+    auto begin = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    long long t;
+    cin >> t;
+    while (t--) {
+        Solve();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
